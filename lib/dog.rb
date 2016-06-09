c401161beca40c3fc1e0a3f47afdc26f98ece2ed
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-09 15:29:42 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+
+
+  def name=(dname)
+    @name = dname
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dbreed)
+    @breed = dbreed
+    #Represents a component of an object
+  end
+
+  def breed
+    @breed
+  end
+
+end
